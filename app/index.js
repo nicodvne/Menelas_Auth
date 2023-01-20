e7a7b0637ec32691e15669d63cd8dfd1fd0448d9
Nicolas Davenne <nico.dvne@gmail.com>
2023-01-20 10:33:45 +0100

    Fix Cors

--- app/index.js
+++ app/index.js
@@ -9,6 +9,18 @@ import './auth/auth.js';
 const app = express();
 app.use(express.json());
 
+app.use((_, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Methods",
+        "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+    );
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, Content-Type, X-Auth-Token"
+    );
+    next();
+});
 
 dotenv.config();
 
